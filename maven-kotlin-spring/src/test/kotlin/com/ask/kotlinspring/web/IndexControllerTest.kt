6d0a33dc4ac0adae4471ed088b99fc2e6c5bbb5c
csh0034 <csh0034@gmail.com>
2022-01-06 00:21:41 +0900

    feat(maven-kotlin-spring): MockMVc Kotlin DSL

--- maven-kotlin-spring/src/test/kotlin/com/ask/kotlinspring/web/IndexControllerTest.kt
+++ maven-kotlin-spring/src/test/kotlin/com/ask/kotlinspring/web/IndexControllerTest.kt
@@ -3,7 +3,9 @@ package com.ask.kotlinspring.web
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
@@ -25,5 +27,17 @@ internal class IndexControllerTest {
             .andExpect(status().isOk)
             .andExpect(content().string("index"))
 
+    }
+
+    @Test
+    fun `index with dsl`() {
+        mockMvc.get("/") {
+            accept(MediaType.TEXT_HTML)
+        }.andExpect {
+            status { isOk() }
+            content { string("index") }
+        }.andDo {
+            print()
+        }
     }
 }
