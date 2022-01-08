be96ee82c129161f924313074a1f80898ed7c81d
csh0034 <csh0034@gmail.com>
2022-01-08 21:02:57 +0900

    feat(maven-kotlin-spring): security 세팅 추가

--- maven-kotlin-spring/src/main/kotlin/com/ask/kotlinspring/config/SecurityConfig.kt
+++ maven-kotlin-spring/src/main/kotlin/com/ask/kotlinspring/config/SecurityConfig.kt
@@ -1,9 +1,14 @@
 package com.ask.kotlinspring.config
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.web.servlet.invoke
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 class SecurityConfig : WebSecurityConfigurerAdapter() {
@@ -17,4 +22,31 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
             .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
             .requestMatchers(PathRequest.toH2Console())
     }
+
+    override fun configure(http: HttpSecurity) {
+        http {
+            headers {
+                frameOptions {
+                    sameOrigin = true
+                }
+            }
+            csrf {
+                disable()
+            }
+            httpBasic {
+                disable()
+            }
+            authorizeRequests {
+                authorize("/admin/**", hasRole("ADMIN"))
+                authorize(anyRequest, permitAll)
+            }
+            formLogin {
+                defaultSuccessUrl(LOGIN_SUCCESS_URL, false)
+            }
+        }
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder =
+        PasswordEncoderFactories.createDelegatingPasswordEncoder()
 }
