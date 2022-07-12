f82af5d4708a1daa6554bedea8fcb4faa70d9c84
csh0034 <csh0034@gmail.com>
2022-07-12 17:20:30 +0900

    feat(webflux-coroutines): init

---
+++ webflux-coroutines/README.md
@@ -0,0 +1,1 @@
+# Webflux Coroutines
---
+++ webflux-coroutines/src/test/kotlin/com/ask/webfluxcoroutines/WebfluxCoroutinesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ask.webfluxcoroutines
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxCoroutinesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
